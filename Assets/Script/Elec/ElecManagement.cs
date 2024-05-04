ecba9335d60d2ffeb5884f4c11802466ff8dfe0d
my9544 <my9544@naver.com>
2024-05-04 15:44:53 +0900

    [FIX} 인터렉션 추가

---
+++ Assets/Script/Elec/ElecManagement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ElecManagement : MonoBehaviour
+{
+    public GameObject blinkingCirclePrefab; 
+    public float blinkInterval = 0.5f; 
+    private float rowposition = -0.6f;
+    private float columnposition = 0.6f;
+    private GameObject[] circles;
+    
+
+    void Start()
+    {
+        circles = new GameObject[9];
+        for (int i = 0; i < 9; i++)
+        {
+            GameObject circle = Instantiate(blinkingCirclePrefab, transform.position, Quaternion.identity);
+            circle.transform.position = new Vector3(rowposition, columnposition, -0.1f);
+            circle.transform.rotation = Quaternion.Euler(-90f, 0f, 0f);
+            circle.SetActive(false);
+            circles[i] = circle;
+            rowposition = rowposition + 0.6f;
+            if (i == 2)
+            {
+                rowposition = -0.6f;
+                columnposition = 0f;
+            }
+            if (i == 5)
+            {
+                rowposition = -0.6f;
+                columnposition = -0.6f;
+            }
+
+
+        }
+
+        InvokeRepeating("ToggleRandomCircle", 0, blinkInterval);
+    }
+
+    void ToggleRandomCircle()
+    {
+        int randomIndex = Random.Range(0, 9);
+        GameObject randomCircle = circles[randomIndex];
+        randomCircle.SetActive(true);
+    }
+
+   
+
+}
