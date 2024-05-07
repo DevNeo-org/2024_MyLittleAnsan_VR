8f6f5cd454e3ef0d92cf67f6142545d68af5367a
hyduck <heemang918@hanyang.ac.kr>
2024-05-07 19:28:24 +0900

    [Feat] 히트박스 이펙트 추가
    - 접촉 시 랜덤으로 이펙트 생성
    - 추후 점수획득 여부로 판정 변경

--- Assets/Script/Interactables/VehicleHitbox.cs
+++ Assets/Script/Interactables/VehicleHitbox.cs
@@ -4,28 +4,61 @@ using UnityEngine;
 
 public class VehicleHitbox : MonoBehaviour
 {
+    [SerializeField] private GameObject correctEffectPrefab;
+    [SerializeField] private GameObject wrongEffectPrefab;
     private bool isCorrect = false; // 오브젝트 위치가 점수 획득 가능한 위치인지 체크
-    void Start()
+    private GameObject instateEffectObj;
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        switch (Random.Range(0, 2))
+        {
+            case 0:
+                isCorrect = true;
+                break;
+            case 1:
+                isCorrect = false;
+                break;
+        }
     }
     public int ObjectHit()
     {
         if (isCorrect)
         {
-            Destroy(transform.parent.gameObject, 0.01f);
+            CorrectEffectPlay();
+            Destroy(transform.parent.gameObject, 0.1f);
             return 0;
         }
         else
         {
-            Destroy(transform.parent.gameObject, 0.01f);
+            WrongEffectPlay();
+            Destroy(transform.parent.gameObject, 0.1f);
             return 1;
         }
+    }
+    private void CorrectEffectPlay()
+    {
+        //파티클프리팹 생성
+        instateEffectObj = Instantiate(correctEffectPrefab, transform.position, Quaternion.identity);
+        ParticleSystem instantEffect = instateEffectObj.GetComponent<ParticleSystem>();
+        //파티클 생성 위치와 크기 설정
+        instateEffectObj.transform.position = transform.position;
+        instateEffectObj.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+        instateEffectObj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        //파티클 재생
+        instantEffect.Play();
+        Destroy(instateEffectObj, 1f);
+    }
+    private void WrongEffectPlay()
+    {
+        //파티클프리팹 생성
+        instateEffectObj = Instantiate(wrongEffectPrefab, transform.position, Quaternion.identity);
+        ParticleSystem instantEffect = instateEffectObj.GetComponent<ParticleSystem>();
+        //파티클 생성 위치와 크기 설정
+        instateEffectObj.transform.position = transform.position;
+        instateEffectObj.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
+        instateEffectObj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        //파티클 재생
+        instantEffect.Play();
+        Destroy(instateEffectObj, 1f);
     }
 }
