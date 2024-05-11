4b5d80dd15d281d704f7ac60c08d2dc4af68063c
hyduck <heemang918@hanyang.ac.kr>
2024-05-11 20:20:54 +0900

    [Feat] 이펙트 위치 조정 및 점수 측정 추가

--- Assets/Script/VehicleInd/VehicleHitbox.cs
+++ Assets/Script/VehicleInd/VehicleHitbox.cs
@@ -6,9 +6,10 @@ public class VehicleHitbox : MonoBehaviour
 {
     [SerializeField] private GameObject correctEffectPrefab;
     [SerializeField] private GameObject wrongEffectPrefab;
-    private bool isCorrect = false; // 오브젝트 위치가 점수 획득 가능한 위치인지 체크
+    public bool isCorrect = false; // 오브젝트 위치가 점수 획득 가능한 위치인지 체크
     private GameObject instateEffectObj;
     private VehicleManager vehicleManager;
+    private Transform finalPoint;
     public int ObjectHit()
     {
         if (isCorrect)
@@ -27,11 +28,12 @@ public class VehicleHitbox : MonoBehaviour
     }
     private void CorrectEffectPlay()
     {
+        finalPoint = transform.parent.GetComponent<HitboxMovement>().GetFinalPoint();
         //파티클프리팹 생성
         instateEffectObj = Instantiate(correctEffectPrefab, transform.position, Quaternion.identity);
         ParticleSystem instantEffect = instateEffectObj.GetComponent<ParticleSystem>();
         //파티클 생성 위치와 크기 설정
-        instateEffectObj.transform.position = transform.position;
+        instateEffectObj.transform.position = finalPoint.position - new Vector3(0,0,0.012f);
         instateEffectObj.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
         instateEffectObj.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
         //파티클 재생
