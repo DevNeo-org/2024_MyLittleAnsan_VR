5177d21bab995aa904d75d0c1139a5b15669e227
hyduck <heemang918@hanyang.ac.kr>
2024-05-15 15:08:43 +0900

    [Feat] 기본 일시정지 메뉴 구현

--- Assets/Script/VehicleInd/VehicleManager.cs
+++ Assets/Script/VehicleInd/VehicleManager.cs
@@ -8,34 +8,38 @@ public class VehicleManager : MonoBehaviour
     [SerializeField] GameObject menu;
     [SerializeField] GameObject leftRayController;
     [SerializeField] GameObject rightRayController;
-    private bool carClear;
+    [SerializeField] private OVRControllerHelper controllerHelperLeft;
+    [SerializeField] private OVRControllerHelper controllerHelperRight;
+    [SerializeField] private GameObject leftWrench;
+    [SerializeField] private GameObject rightWrench;
+
+    HitboxSpawner spawner;
     private int score;
-    private float timer;
     private bool isMenuOn = false;
     DataManager dataManager;
     void Start()
     {
         score = 0;
         dataManager = FindAnyObjectByType<DataManager>();
+        menu.SetActive(false);
+        spawner = FindAnyObjectByType<HitboxSpawner>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.One) || OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Three) || OVRInput.GetDown(OVRInput.Button.Four))
+        if (!isMenuOn)
         {
-            if (isMenuOn)
+            if (OVRInput.GetDown(OVRInput.Button.One) ||  OVRInput.GetDown(OVRInput.Button.Three))
             {
-                Time.timeScale = 1;
-                isMenuOn = false;
-                menu.SetActive(false); leftRayController.SetActive(false); rightRayController.SetActive(false);
+                rightRayController.SetActive(true);
             }
-            else
+            else if (OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Four))
             {
-                Time.timeScale = 0;
-                isMenuOn = true;
-                menu.SetActive(true); leftRayController.SetActive(true); rightRayController.SetActive(true);
+                leftRayController.SetActive(true);
             }
+            OpenMenu();
+            
         }
         if (score > 10)
         {
@@ -54,6 +58,45 @@ public class VehicleManager : MonoBehaviour
     public void LoadScene()
     {
         Time.timeScale = 1;
-        SceneManager.LoadScene("CityDesign");
+        SceneManager.LoadScene("Title");
+    }
+    public void RestartScene()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("AutomobIndScene");
+    }
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
+    }
+    public void CloseMenu()
+    {
+        Time.timeScale = 1;
+        isMenuOn = false;
+        menu.SetActive(false); leftRayController.SetActive(false); rightRayController.SetActive(false);
+        if (spawner.leftWrenchOn)
+        {
+            leftWrench.gameObject.SetActive(true);
+            controllerHelperLeft.m_showState = OVRInput.InputDeviceShowState.ControllerNotInHand;
+        }
+        if(spawner.rightWrenchOn)
+        {
+            rightWrench.gameObject.SetActive(true);
+            controllerHelperRight.m_showState = OVRInput.InputDeviceShowState.ControllerNotInHand;
+        }
+    }
+    private void OpenMenu()
+    {
+        Time.timeScale = 0;
+        isMenuOn = true;
+        menu.SetActive(true); 
+        leftWrench.gameObject.SetActive(false);
+        rightWrench.gameObject.SetActive(false);
+        controllerHelperLeft.m_showState = OVRInput.InputDeviceShowState.ControllerInHandOrNoHand;
+        controllerHelperRight.m_showState = OVRInput.InputDeviceShowState.ControllerInHandOrNoHand;
     }
 }
