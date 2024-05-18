058969ab7cbfdff7f2ed31ee2250024a778c9ee9
JeonWooJin14 <jokey1344@gmail.com>
2024-05-18 23:26:42 +0900

    [Fix] 페인트 발사 오류 수정
    연속 발사 시 페인트볼 충돌 위치와 색칠 위치가 다른 문제 수정

--- Assets/Script/TextileIndustry/Gun.cs
+++ Assets/Script/TextileIndustry/Gun.cs
@@ -12,103 +12,72 @@ public class Gun : MonoBehaviour
 {
     [SerializeField] ParticleSystem[] _bucketPop;
 
-    [SerializeField] GameObject ball;
-    private ParticleSystem particle;
-    private Renderer particleRenderer;
-    [SerializeField] Material[] paintMats;
-
-    [SerializeField] GameObject gun;
-    [SerializeField] GameObject gunModel;
+    [SerializeField] GameObject gunObject;
     private Renderer gunRenderer;
-    private Rigidbody rigid;
 
-    [SerializeField] private int penSize = 35;
+    [SerializeField] Material[] gunMaterials;
+    [SerializeField] Material[] ballMaterial;
 
-    private Color[] _color;
-    private Whiteboard _whiteboard;
-    [SerializeField] Material[] mats;
-    [SerializeField] Material[] gunMats;
-    private Vector3 shootPos, shootDir;
-    private RaycastHit _touch;
-    private Vector2 _touchPos;
+    [SerializeField] private GameObject board;
 
-    [SerializeField] GameObject board;
+    [SerializeField] private GameObject ball;
+    private Ball ballCS;
+    private Renderer ballRenderer;
+    public float ballSpeed = 15f;
+    public float shootRate = 0.2f;
+    private float nextShoot;
+
+    public int colorCode;
 
 
     void Start()
     {
-        gunRenderer = gunModel.GetComponent<Renderer>();
-        rigid = gun.GetComponent<Rigidbody>();
-        _color = Enumerable.Repeat(mats[0].color, penSize * penSize).ToArray();
-        particle = ball.GetComponent<ParticleSystem>();
-        particleRenderer = particle.GetComponent<Renderer>();
+        gunRenderer = gunObject.GetComponent<Renderer>();
+        ballRenderer = ball.GetComponent<Renderer>();
+        ballCS = ball.GetComponent<Ball>();
+
+        // initiate color code
+        gunRenderer.material = gunMaterials[0];
+        ballRenderer.material = ballMaterial[0];
+        ballCS.ChangeColorCode(0);
+        colorCode = 0;
     }
     private void FixedUpdate()
     {
-        if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+        // input 
+
+        /* if (Get(Button.PrimaryIndexTrigger))
         {
+            nextShoot = Time.time + shootRate;
             Shoot();
         }
-        ball.transform.rotation = gun.transform.rotation;
+        */
+
+        if (Input.GetMouseButtonDown(0) && Time.time > nextShoot)
+        {
+            nextShoot = Time.time + shootRate;
+            Shoot();
+        }
     }
 
+    // shoot paint ball
     private void Shoot()
     {
-        shootPos = rigid.transform.position;
-        shootDir = Camera.main.transform.forward;
-        particle.Play();
-    }
-    public void Draw()
-    {
-        if (Physics.Raycast(shootPos, shootDir, out _touch, 50f))
-        {
-            if (_touch.transform.CompareTag("Whiteboard"))
-            {
-                if (_whiteboard == null)
-                {
-                    _whiteboard = _touch.transform.GetComponent<Whiteboard>();
-                }
-
-                _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
-
-                var x = (int)(_touchPos.x * _whiteboard.textureSize.x - (penSize / 2));
-                var y = (int)(_touchPos.y * _whiteboard.textureSize.y - (penSize / 2));
-
-                if (y < 0 || y > _whiteboard.textureSize.y || x < 0 || x > _whiteboard.textureSize.x)
-                {
-                    return;
-                }
-
-                for (int i=-1; i<=1; i++)
-                {
-                    for (int j=-2; j<=2; j++)
-                    {
-                        _whiteboard.texture.SetPixels(x + i*(penSize), y + j * (penSize), penSize, penSize, _color);
-                    }
-                }
-                _whiteboard.texture.SetPixels(x + (penSize) / 2, y + 3 * (penSize), penSize, penSize, _color);
-                _whiteboard.texture.SetPixels(x - (penSize) / 2, y + 3 * (penSize), penSize, penSize, _color);
-                _whiteboard.texture.SetPixels(x + (penSize) / 2, y - 3 * (penSize), penSize, penSize, _color);
-                _whiteboard.texture.SetPixels(x - (penSize) / 2, y - 3 * (penSize), penSize, penSize, _color);
-
-                _whiteboard.texture.Apply();
-
-                return;
-            }
-        }
-
-        _whiteboard = null;
-
+        GameObject paintBall = Instantiate(ball, gunObject.transform.position, gunObject.transform.rotation);
+        paintBall.GetComponent<Rigidbody>().velocity = paintBall.transform.forward * ballSpeed;
     }
 
+    // change color of paint and gun
     public void ChangeColor(int code)
     {
         _bucketPop[code].Play();
-        _color = Enumerable.Repeat(mats[code].color, penSize * penSize).ToArray();
-        gunRenderer.material = gunMats[code];
-        particleRenderer.material = paintMats[code];
-        board.GetComponent<Whiteboard>().ChangeCode(code);
+        gunRenderer.material = gunMaterials[code];
+        ballRenderer.material = ballMaterial[code];
+        ballCS.ChangeColorCode(code);
+        colorCode = code;
     }
+
+    // check Trigger with paint bucket
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "BlueBucket")
