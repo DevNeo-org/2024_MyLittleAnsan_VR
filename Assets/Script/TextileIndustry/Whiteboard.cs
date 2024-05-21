4ead610db100f69cd3c560e8774dda37fcef1c8f
JeonWooJin14 <jokey1344@gmail.com>
2024-05-21 23:51:48 +0900

    [Fix] 옷 material 수정
    색칠 색 연한 문제 수정

--- Assets/Script/TextileIndustry/Whiteboard.cs
+++ Assets/Script/TextileIndustry/Whiteboard.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class Whiteboard : MonoBehaviour
 {
     public Texture2D texture;
-    public Vector2 textureSize = new Vector2(2048,2048);
+    public Vector2 textureSize = new Vector2(512,512);
     // Start is called before the first frame update
     void Start()
     {
         var r = GetComponent<Renderer>();
         texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
         r.material.mainTexture = texture;
-        r.material.color = new Color(211 / (float)256, 210 / (float)256, 187 / (float)256, 20 / (float)256);
+        r.material.color = new Color (195/(float)256, 201 / (float)256, 213 / (float)256, 78 / (float)256);
     }
 }
